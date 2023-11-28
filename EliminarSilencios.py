8b874100c0663f14586f152e3013c59fd1e0f418
Jose Manuel Carrichi <josemanuelcarrichi16@gmail.com>
2023-11-28 01:47:22 -0600

    Se ajusta el umbral para la detección de voz

--- EliminarSilencios.py
+++ EliminarSilencios.py
@@ -3,10 +3,9 @@ import numpy as np
 import soundfile as sf
 
 # Lectura del audio
-#Fss, Audio = wav.read('Audios/EnciendeLaLuz.wav')
-Audio, Fss = sf.read('Audios/EnciendeLaLuz.wav') 
+Audio, sample_rate = sf.read('Audios/Vian_EnciendeLaLuz.wav') 
 
-tam = len(Audio)
+#tam = len(Audio)
 yn = Audio**2 # Calcular la energía de la señal
 yne = np.log(yn) # Pasar a escala logarítmica
 yneAux = np.copy(yne)
@@ -31,9 +30,6 @@ hyne[Ind1] = 0
 hyne[Ind2] = 0
 hyne[Ind3] = 0
 
-Ind1 -= 1
-Ind2 -= 1
-Ind3 -= 1
 
 #Graficar el histograma
 plt.figure()
@@ -45,15 +41,16 @@ plt.show()
 Q = ((-((10 - Ind1) + 0.5) + aux1)+ (-((10 - Ind2) + 0.5) + aux1)+ (-((10 - Ind3) + 0.5) + aux1)) / 3
 
 audio = Q +aux
-k3 = Q - (audio*(0.375/3))
-Humbralk3 = np.zeros(tam)
+#k3 = (Q - (audio*(0.375/3)))+1
+k3 = (Q - (audio*(0.375/1.6)))
+Humbralk3 = np.zeros(len(Audio))
 Humbralk3[yne>=k3] = 1 # Los valores que superan el humbral se colocan en 1
 
 r = np.where(Humbralk3 == 1)[0]
 aux = len(r)
 
 for s in range(aux - 1):
-    if (r[s + 1] - r[s]) <= 500:
+    if (r[s + 1] - r[s]) <= 1000:
         Humbralk3[r[s]:r[s + 1] + 1] = 1
 
 
@@ -72,3 +69,6 @@ plt.figure()
 plt.plot(Audio)
 plt.plot(Humbralk3)
 plt.show()
+
+# Guardar el audio final
+sf.write('Output_Audio/Voice_without_silence.wav',AudioClean,sample_rate)
